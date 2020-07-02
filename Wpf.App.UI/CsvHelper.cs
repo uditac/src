9740463b4a478cc6c12d6e0e7d93591842fd64e4
Udita Chakraborty <mail2udita@yahoo.com>
2020-07-02 11:10:37 +0200

    Add project files.

---
+++ Wpf.App.UI/CsvHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Wpf.App.UI
+{
+    public static class CsvHelper
+    {
+        public static DataView GetCSVContent(string filePath)
+        {
+            DataTable dt = new DataTable();
+            string[] lines = System.IO.File.ReadAllLines(filePath);
+
+            if (lines.Length > 0)
+            {
+                string firstLine = lines[0];
+                string[] headerLabels = firstLine.Split(';');
+                foreach (string headerWord in headerLabels)
+                {
+                    dt.Columns.Add(new DataColumn(Regex.Replace(headerWord, @"\s+", "")));
+                }
+                dt.Columns.Add(new DataColumn("Color"));
+                for (int r = 1; r < lines.Length; r++) 
+                {
+                    string[] dataWords = lines[r].Split(';');
+                    DataRow dr = dt.NewRow();
+                    int columnIndex = 0;
+                    foreach (string headerWord in dt.Columns.Cast<DataColumn>().Select(col=> col.ColumnName).ToArray())
+                    {
+                        if (headerWord == "InStock")
+                        {
+                            if (dataWords[columnIndex] == "yes")
+                                dr[headerWord] = true;
+                            else if(dataWords[columnIndex] == "no")
+                                dr[headerWord] = false;
+                        }
+                        else if(headerWord == "Color")
+                        {
+                            Random randomGen = new Random();
+                            KnownColor[] names = (KnownColor[])Enum.GetValues(typeof(KnownColor));
+                            KnownColor randomColorName = names[randomGen.Next(names.Length)];
+                            dr[headerWord] = randomColorName.ToString();
+
+                        }
+                        else
+                            dr[headerWord] = dataWords[columnIndex];
+                        columnIndex++;
+                    }
+                    dt.Rows.Add(dr);
+                }
+            }
+
+            return dt.AsDataView();
+        }
+    }
+}
---
+++ Wpf.App.UI/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Wpf.App.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDlg = new OpenFileDialog();
+            Nullable<bool> result = openFileDlg.ShowDialog();
+            if (result == true)
+            {
+                txtFile.Text = openFileDlg.FileName;
+                BindGrid(openFileDlg);
+                HighlightRows();
+            }
+        }
+
+        private void BindGrid(OpenFileDialog file)
+        {
+            dgBooks.ItemsSource = CsvHelper.GetCSVContent(file.FileName);
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            List<DataRowView> lstDelete = new List<DataRowView>();
+            foreach(DataRowView rowview in dgBooks.Items)
+            {
+                if (rowview.Row.Field<string>("InStock") != "True")
+                {
+                    lstDelete.Add(rowview);
+                }
+            }
+            foreach(DataRowView rowview in lstDelete)
+            {
+                DataView dataView = dgBooks.ItemsSource as DataView;
+                dataView.Table.Rows.Remove(rowview.Row);
+            }
+        }
+
+        private void HighlightRows()
+        {
+            
+            for (int i = 0; i < dgBooks.Items.Count; i++)
+            {
+                dgBooks.UpdateLayout();
+                dgBooks.ScrollIntoView(dgBooks.Items[i]);
+                DataRowView rowview = dgBooks.Items[i] as DataRowView;
+                DataGridRow row = (DataGridRow)dgBooks.ItemContainerGenerator.ContainerFromIndex(i);
+                var val = rowview.Row.Field<string>("InStock");
+                if (val != null && val.Equals("False"))
+                {
+                    object item = dgBooks.Items[i];           
+                    var converter = new System.Windows.Media.BrushConverter();
+                    var brush = (Brush)converter.ConvertFromString("#FFFFFF90");
+                    row.Background = brush;
+                   
+                }
+
+               
+
+            }
+
+
+      
+
+        }
+    }
+}
